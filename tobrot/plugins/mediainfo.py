022a865e24bc8eaccd64e893d72042d705e7975a
Pachukk <88023964+Pachukk@users.noreply.github.com>
2022-06-12 16:11:11 +0530

    Update mediainfo.py

--- tobrot/plugins/mediainfo.py
+++ tobrot/plugins/mediainfo.py
@@ -20,7 +20,7 @@ from tobrot.helper_funcs.bot_commands import BotCommands
 def post_to_telegraph(a_title: str, content: str) -> str:
     """ Create a Telegram Post using HTML Content """
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "CB"
+    auth_name = "Myran"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=a_title,
